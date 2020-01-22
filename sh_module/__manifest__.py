80cc9b5e8575d04f67b28646017d4c5dfc79082b
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-01-22 15:11:29 +0100

    add sh

---
+++ sh_module/__manifest__.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "DynApps Odoo.sh testing module",
+    "version": "13.0.1.0.0",
+    "license": "",
+    "author": "DynApps NV",
+    "category": "Odoo",
+    "depends": ["account"],
+    "data": ["views/templates.xml"],
+    "installable": True,
+}
